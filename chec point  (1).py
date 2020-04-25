5c2ee1f91d5ffbb5f6a313f7ca420fe4317017f5
tassnime <64094598+tassnime@users.noreply.github.com>
2020-04-25 13:42:33 +0100

    Add files via upload

---
+++ chec point  (1).py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(2000,3201,1):
+    if (i%7==0) and (i%5!=0):
+        print(i)
+
+
+# In[2]:
+
+
+def func(n):
+    for i in range(1,n,1):
+         n= n*i
+    return n
+print(func(5))
+
+
+# In[ ]:
+
+
+for i in range (1,n+1):
+def f(x):
+    return x=n-1
+n=5
+ dict[i]=x*x
+    print (dict) 
+
+
+# In[3]:
+
+
+#python sqrt
+import math 
+Q = math.sqrt((2*C*d)/ h)
+C = 50
+h = 30
+ def func (d):
+    d=50
+print(round (Q,0))
+
+
+# In[4]:
+
+
+def missing (str, n):
+      first_part = str[:n] 
+      last_part = str[n+1:]
+      return first_part + last_part
+print (missing('kitten', 1))  
+print (missing('kitten', 0)) 
+print (missing('kitten', 4))
+
+
+# In[5]:
+
+
+raw = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+chr(65)
+ord('A')
+ord('k')
+ord('k') + 2
+chr(ord('k') + 2)
+chr(ord('z') + 2)
+(ord('z') + 2) - ord('a')
+((ord('z') + 2) - ord('a')) % 26
+chr(((ord('z') + 2) - ord('a')) % 26 + ord('a'))
+''.join([chr(((ord(s) + 2) - ord('a')) % 26 + ord('a')) if s >= 'a' and s <= 'z' else s for s in raw])
+
+
+# In[ ]:
+
+
+import numpy as np
+x= np.arange(6).reshape(3, 2)
+print("Original array elements:")
+print(x)
+print("Array to list:")
+print(x.tolist())
+
